47edc5ce2746ab357b403e5b8510d08317cb2731
nikaytos <nikaytos@gmail.com>
2025-08-16 22:48:55 +0300

    Format recipe response

--- src/components/ClaudeRecipe.jsx
+++ src/components/ClaudeRecipe.jsx
@@ -1,9 +1,11 @@
+import ReactMarkdown from 'react-markdown'
+
 export default function ClaudeRecipe(props) {
     return (
         <section>
             <h2>Chef Claude Recommends:</h2>
             <article className="suggested-recipe-container" aria-live="polite">
-                {props.text}
+                <ReactMarkdown>{props.text}</ReactMarkdown>
             </article>
         </section>
     )
