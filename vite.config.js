2fcb6333fe6fb9118a4959d251cf8196878bdd28
nikaytos <nikaytos@gmail.com>
2025-08-17 19:57:03 +0300

    Changed name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/scrimba-react-project-3/',
+  base: '/chef-claude-srp3/',
   plugins: [react()],
   server: {
     proxy: {
